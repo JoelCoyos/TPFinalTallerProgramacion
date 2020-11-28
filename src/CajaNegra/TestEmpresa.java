63b095dc39576a7ccfef16da44cce0b3423bb25c
JosefinaOller <64923768+JosefinaOller@users.noreply.github.com>
2020-11-28 15:22:17 -0300

    Agregué las carpetas CajaNegra y CajaBlanca. Mudé los tests que estaban en la carpeta Tests, dicha carpeta la eliminé

--- src/CajaNegra/TestEmpresa.java
+++ src/CajaNegra/TestEmpresa.java
@@ -1,4 +1,4 @@
-package test;
+package CajaNegra;
 
 import static org.junit.Assert.*;
 
---
+++ src/CajaNegra/TestTitularFisico.java
@@ -0,0 +1,24 @@
+package CajaNegra;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestTitularFisico {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
