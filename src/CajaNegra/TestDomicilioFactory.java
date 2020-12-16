9082964b72b7c9dbb8e6f16da1235d0f7aa6a715
unknown <freestyle319@gmail.com>
2020-12-16 15:15:49 -0300

    Correccion test DomicilioFactory

--- src/CajaNegra/TestDomicilioFactory.java
+++ src/CajaNegra/TestDomicilioFactory.java
@@ -35,6 +35,7 @@ public class TestDomicilioFactory {
 	public void testGetDomicilio1() {
 		Titular titular= new TitularFisico("Mario Donato",39520966,"Efectivo");
 		titular.addDomicilio("Mitre 455", "Internet100", true, false, true);
+		this.e.addTitular(titular);
 		try {
 			Domicilio d= DomicilioFactory.getDomicilio("Saavedra 9833", "Internet100", false, false, true);
 			assertEquals("El Domicilio generado no es correcto", "Saavedra 9833", d.getDireccion());
@@ -51,6 +52,7 @@ public class TestDomicilioFactory {
 	public void testGetDomicilio2() {
 		Titular titular= new TitularFisico("Marcus Nogueira",40122699,"Efectivo");
 		titular.addDomicilio("San Lorenzo 9652", "Internet500", true, false, true);
+		this.e.addTitular(titular);
 		try {
 			Domicilio d= DomicilioFactory.getDomicilio("San Lorenzo 9652", "Internet100", false, false, true);
 			fail("No se deberia crear la instancia de domicilio por que ya existe para un titular de una empresa");
