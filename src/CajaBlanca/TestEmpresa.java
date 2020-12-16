59d1c54539403fda64e40b60aebd25c6a35ece22
unknown <freestyle319@gmail.com>
2020-12-16 17:20:59 -0300

    Agregado lo que faltaba de TestEmpresa en Caja Blanca

--- src/CajaBlanca/TestEmpresa.java
+++ src/CajaBlanca/TestEmpresa.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import Modelo.Empresa;
 import Modelo.Titular;
 import Modelo.TitularFisico;
+import Modelo.TitularJuridico;
 
 public class TestEmpresa {
 
@@ -52,8 +53,8 @@ public class TestEmpresa {
 		titular1.addDomicilio("Salta 563", "Internet100", true, false, true);
 		this.empresa.addTitular(titular1);
 		Titular titular2= new TitularFisico("Andres Juan",41050236,"Tarjeta"); 
-		titular1.addDomicilio("Barcelona 5152", "Internet100", false, false, true);
-		titular1.addDomicilio("Rosales 1291", "Internet500", true, false, true);
+		titular2.addDomicilio("Barcelona 5152", "Internet100", false, false, true);
+		titular2.addDomicilio("Rosales 1291", "Internet500", true, false, true);
 		this.empresa.addTitular(titular2);
 		if (titulares.size()==0)
 			fail("La empresa no tiene titulares");
@@ -61,6 +62,48 @@ public class TestEmpresa {
 			empresa.detalle();
 		}
 		
+	}
+	
+	/**
+	 * Se solicita el duplicado de un Titular Fisico 
+	 */
+	
+	@Test
+	public void testSolicitarDuplicado1() {
+		this.empresa= Empresa.getInstance();
+		Titular titular= new TitularFisico("Enzo Barria",41120666,"Tarjeta"); 
+		titular.addDomicilio("Santa Clara 5152", "Internet500", false, false, true);
+		titular.addDomicilio("Santiago del Estero 633", "Internet500", true, false, true);
+		this.empresa.addTitular(titular);
+		int nroId= titular.getNroIdentacion();
+		Titular clonado= this.empresa.solicitaDuplicado(nroId);
+		if (clonado==null) {
+			fail("El titular se debio clonar correctamente por que estaba registrado en la empresa y era fisico");
+		}
+		else {
+			assertEquals("El nombre del titular clonado es  incorrecto",titular.getNombre() ,clonado.getNombre());
+			assertEquals("El DNI del titular clonado es incorrecto",titular.getDni() ,clonado.getDni());
+			assertEquals("El tipo de pago del titular clonado es incorrecto",titular.getTipoDePago() ,clonado.getTipoDePago());
+		}
+		
+	}
+	
+	/**
+	 * Se solicita el duplicado de un Titular Juridico 
+	 */
+	
+	@Test
+	public void testSolicitarDuplicado2() {
+		
+		this.empresa= Empresa.getInstance();
+		Titular titular= new TitularJuridico("Mike Wazowsky S.A",42336699,"Efectivo"); 
+		titular.addDomicilio("Guemes 2559", "Internet500", true, true, true);
+		this.empresa.addTitular(titular);
+		int nroId= titular.getNroIdentacion();
+		Titular clonado= this.empresa.solicitaDuplicado(nroId);
+		if (clonado!=null) {
+			fail("No se debio clonar por que el titular era juridico");
+		}
 	}
 
 }
